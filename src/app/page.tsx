32713a017bf56f75c7ee3001d6eea508b798663a
UnderCoverTable <sameed.gilani@gmail.com>
2025-09-14 22:26:09 +0500

    fixed default route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+// pages/index.tsx
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: "/game", // 👈 Change this
+      permanent: false,
+    },
+  };
+}
 
-export default function Home() {
-  return <>Hello</>;
+export default function Index() {
+  return null;
 }
